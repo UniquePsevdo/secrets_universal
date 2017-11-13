561583309cd2233dfb56e9e35ae1a5a964128b58
Volodymyr <wladimir.sidorow@gmail.com>
2017-11-13 22:56:07 +0200

    +

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import { Location } from '@angular/common';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {TranslateService} from "@ngx-translate/core";
+import {LocalizeParser, LocalizeRouterModule, LocalizeRouterSettings} from 'localize-router';
+import {TranslateModule} from '@ngx-translate/core';
+import {HttpClient} from "@angular/common/http";
+import {LocalizeRouterHttpLoader} from "localize-router-http-loader";
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-  ],
-  imports: [
-    BrowserModule.withServerTransition({appId: 'my-app'}),
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full'},
-      { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
-      { path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
-    ])
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+    ],
+    imports: [
+        BrowserModule.withServerTransition({appId: 'my-app'}),
+        LocalizeRouterModule.forRoot([
+            {path: '', component: HomeComponent, pathMatch: 'full'},
+            {path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
+            {path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
+        ], {
+            parser: {
+                provide: LocalizeParser,
+                useFactory: HttpLoaderFactory,
+                deps: [TranslateService, Location, LocalizeRouterSettings, HttpClient]
+            }
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
+
+export function HttpLoaderFactory(translate: TranslateService, location: Location, settings: LocalizeRouterSettings, http: HttpClient) {
+    return new LocalizeRouterHttpLoader(translate, location, settings, http, 'assets/locales.json');
+}
+
