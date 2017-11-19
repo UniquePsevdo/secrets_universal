583ba27b4bca03e71d221c72b51ea18acae80b4c
Volodymyr <wladimir.sidorow@gmail.com>
2017-11-19 21:24:47 +0200

    /egect:

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,6 +1,16 @@
-/*
 import {AngularCompilerPlugin} from '@ngtools/webpack';
-exports = {
+module.exports = {
+    entry: {
+        server: './src/app/app.server.module.ts'
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    target: 'node',
+    output: {
+        path: path.join(__dirname, "dist/server"),
+        filename: 'server.js'
+    },
     module: {
         rules: [
             {
@@ -16,4 +26,48 @@ exports = {
             entryModule: './src/app/app.server.module#AppServerModule'
         })
     ]
+}
+
+/*
+const ngtools = require('@ngtools/webpack');
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
+module.exports = {
+    entry: {
+        server: './src/app/app.server.module.ts'
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    target: 'node',
+    output: {
+        path: path.join(__dirname, "dist/server"),
+        filename: 'server.js'
+    },
+    plugins: [
+        new ngtools.AotPlugin({
+            tsConfigPath: './tsconfig.json'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(scss|html|png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                use: 'raw-loader'
+            },
+            { test: /\.ts$/,  loader: require.resolve('@ngtools/webpack') },
+            {
+                test: /\.(png|jpg|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url?limit=512&&name=[path][name].[ext]?[hash]'
+            },
+            { test: /\.scss$/, use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }] }
+        ]
+    }
 }*/
